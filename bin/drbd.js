429a1ca05436640c0287948dda549487cf1e14a9
Mark Olliver <mpo@mpo-hn.thermeon.com>
2012-10-09 14:32:04 +0000

    working metrics - now working on options

--- bin/drbd.js
+++ bin/drbd.js
@@ -1,17 +1,52 @@
 #!/usr/bin/env node
+var optparse = require('optparse');
 
-//var drbdProc = require('../lib/drbdProc');
-//var util = require('util');
+var SWITCHES = [
+  ['-d', '--debug', 'Enables debug mode'],
+  ['-h', '--help', 'Shows this help section'],
+  ['-m', '--metrics', 'Output Metrics'],
+  ['-s', '--scheme', 'Metrics Naming Scheme']
+  ];
 
-/*
-drbdProc.doit(function(callback){
-  var drbd = callback;
-  console.log(drbd);
-}); 
-*/
+var parser = new optparse.OptionParser(SWITCHES), print_summary = true,
+  first_arg;
+parser.banner = 'Usage: drbd.js [options]';
 
-var drbdOutput = require('../lib/drbdOutput');
+var options = {
+  debug: false,
+  scheme: ''
+};
 
-drbdOutput.doit(function(callback){
-  console.log(callback);
+parser.on(0, function(value) {
+  first_arg = value;
 });
+
+parser.on('debug', function() {
+  options.debug = true;
+});
+
+parser.on('help', function() {
+  console.log(parser.toString());
+  print_summary = false;
+});
+
+parser.on('scheme', function(value) {
+  options.scheme = value;
+  console.log("Scheme name set to: " + option.scheme)
+});
+
+parser.on('metrics', function() {
+  console.log("printing metrics");
+  var drbdOutput = require('../lib/drbdOutput');
+  drbdOutput.doit(function(callback){
+    console.log(callback);
+  });
+});
+
+parser.parse(process.ARGV);
+
+if (print_summary) {
+  console.log("First non-switch argument is: " + first_arg);
+  console.log("Debug mode is set to: " + options.debug);
+  console.log("Scheme is set to: " + options.scheme);
+}
