d99e7fcb12190e90ad2454dd7b2fc4d9a4c3322d
Mark Olliver <mpo@mpo-hn.thermeon.com>
2012-10-09 11:21:18 +0000

    a bit further on with drbd metrics stuck on async

--- bin/drbd.js
+++ bin/drbd.js
@@ -1,25 +1,17 @@
 #!/usr/bin/env node
 
-var drbdProc = require('../lib/drbdProc');
-var os = require('os');
-var util = require('util');
+//var drbdProc = require('../lib/drbdProc');
+//var util = require('util');
 
+/*
 drbdProc.doit(function(callback){
   var drbd = callback;
-  console.log(drbd.drbdinfo.version);
-  console.log(drbd.drbd0.state.cs);
-  console.log(drbd.drbd1.state.cs);
+  console.log(drbd);
 }); 
-/*
-
-function output(){
-  var prefix = new String;
-  var prefixHost = new String;
-  prefixHost = os.hostname();
-  prefixHost = prefixHost.split('.');
-  prefix = "australia.";
-  prefix = prefix.concat(prefixHost[0]);
-  console.log(prefix);
-}
-
 */
+
+var drbdOutput = require('../lib/drbdOutput');
+
+drbdOutput.doit(function(callback){
+  console.log(callback);
+});
