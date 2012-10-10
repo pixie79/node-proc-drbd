61cc90b481ceb8635ab4560d3b1baf7c6be33a38
Mark Olliver <mpo@mpo-hn.thermeon.com>
2012-10-10 09:24:39 +0000

    update - fully working drbd metrics

--- bin/drbd.js
+++ bin/drbd.js
@@ -1,25 +1,27 @@
 #!/usr/bin/env node
+"use strict";
 var Getopt = require('node-getopt');
+var os = require('os');
 
-getopt = new Getopt([
+var getopt = new Getopt([
   ['d', 'debug'],
   ['h', 'help'],
   ['m', 'metrics'],
   ['s', 'scheme', Getopt.HAS_ARGUMENT, Getopt.SINGLE_ONLY]
-  ]);
+]);
 
-opt = getopt.parse(process.argv.slice(2));
+var opt = getopt.parse(process.argv.slice(2));
 
 if (opt.options.metrics) {
-  var scheme = "";
+  var scheme = os.hostname();
   if (opt.options.scheme) {
     scheme = opt.options.scheme;
   }
   var drbdOutput = require('../lib/drbdOutput');
-  drbdOutput.doit(function(callback){
+  drbdOutput.doit(function (callback) {
     var output = callback;
-    output.forEach(function(item) {
-      console.log(scheme + "." + item);
+    output.forEach(function (item) {
+      console.log(scheme + ".disk." + item);
     });
   });
-};
+}
